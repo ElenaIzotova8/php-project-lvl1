02ca472d6500bb12c2c9b5d2d903386ed836a4b6
Elena Izotova <eleizotova@ya.ru>
2020-04-13 17:05:24 +0300

    step 5

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,26 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function run()
+function welcome($instruction)
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
+    line($instruction);
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
 }
+
+function question($quest, $correctAns)
+{
+    line("Question: %s", $quest);
+    $answer = prompt('Your answer');
+    if ($answer !== $correctAns) {
+        $wrong = "'" . $answer . "' is wrong answer ;(. Correct answer was '" . $correctAns . "'.";
+        line($wrong);
+        return false;
+    } else {
+        line('Correct!'); 
+        return true;
+    }
+}
+
